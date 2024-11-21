6932864a4cf32f86805c492974e1fe848d72e69d
Krejcicek <94050047+Krejcicek@users.noreply.github.com>
2024-11-21 08:32:26 +0100

    first commit

---
+++ S-Krejčíček/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Singleton
+{
+    static Singleton singl; 
+
+    private Singleton() { } 
+
+    public static Singleton Dej()
+    {
+        if (singl == null)
+        {
+            singl = new Singleton();
+            Console.WriteLine("instance udelana.");
+        }
+        return singl;
+    }
+
+    public void Ukaz()
+    {
+        Console.WriteLine("Singleton");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var prvni = Singleton.Dej();
+        prvni.Ukaz();
+
+        var druhy = Singleton.Dej();
+
+        if (prvni == druhy)
+        {
+            Console.WriteLine("Opakuje se");
+        }
+        else
+        {
+            Console.WriteLine("Chyba");
+        }
+    }
+}
